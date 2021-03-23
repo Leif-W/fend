5677acf3358842a21c6a1ca1e75513343e2686e3
Leif-W <Leif-W@noreply.users.github.com>
2021-03-23 16:19:40 -0400

    feat: Add initial nav build

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,57 +1,65 @@
+'use strict';
+
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Define Global Variables
- * 
-*/
+ *
+ */
 
+const navbarList = document.getElementById('navbar__list');
+let sections = document.getElementsByName('section');
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
-
-
+ *
+ */
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
 
+for (let i = 0; i < sections.length; i++) {
+  let li_ = document.createElement('li');
+  let a_ = document.createElement('a');
+
+  a_.href = '#' + sections[i].id;
+  a_.textContent = sections[i].dataset.nav;
+
+  li_.appendChild(a_);
+  navbarList.appendChild(li_);
+}
 
 // Add class 'active' to section when near top of viewport
 
-
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
